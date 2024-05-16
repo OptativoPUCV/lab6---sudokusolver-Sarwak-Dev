5f54e952891ff307bd8b851533b6150990cf0ecb
Your Name <you@example.com>
2024-05-16 15:53:56 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,8 +43,19 @@ void print_node(Node* n){
     printf("\n");
 }
 
-int is_valid(Node* n){
-
+int is_valid(Node* n) {
+    int row[9][9] = {0}, col[9][9] = {0}, box[9][9] = {0};
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (n->sudo[i][j] != 0) {
+                int num = n->sudo[i][j] - 1;
+                int k = (i / 3) * 3 + j / 3;
+                if (row[i][num] || col[j][num] || box[k][num])
+                    return 0;
+                row[i][num] = col[j][num] = box[k][num] = 1;
+            }
+        }
+    }
     return 1;
 }
 
